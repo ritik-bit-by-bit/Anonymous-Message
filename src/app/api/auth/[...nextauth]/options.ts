e62c127226813dd97036f91b76bc4f4fd9e29f2a
ritik-bit-by-bit <ritikroshanyadav9696@gmail.com>
2025-06-30 15:11:19 +0530

    auth flow completed

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,74 @@
+import {NextAuthOptions} from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcryp from "bcryptjs"
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/user";
+import bcrypt from "bcryptjs";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            id:"credentials",
+            name: "Credentials",
+            credentials: {
+                Email: { label: "Email", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+              },
+              async authorize(credentials, req): Promise<any> {
+                await dbConnect()
+                try {
+                 const user= await UserModel.findOne({
+                    $or:[
+                        {email:credentials?.Email},
+                    ]
+                   })
+                   if(!user){
+                    throw new Error("No user found")
+                   }
+                   if(!user.isVerified){
+                    throw new Error("Please verify your account")
+                   }
+                 const passwordMatch =  await bcrypt.compare(credentials?.password as string,user.password)
+                 if(!passwordMatch){
+                    throw new Error("Invalid credentials")
+                 }
+                 else{
+                    return user;
+                 }
+                   
+                } catch (error) {
+                    throw new Error("Something went wrong")
+                }
+            }
+        })       
+    ],
+    pages:{
+        signIn:"/sign-in",
+        error:"/login",
+        signOut:"/",
+    },
+    session:{
+        strategy:"jwt"
+    },
+    secret:process.env.NEXTAUTH_SECRET,
+    callbacks:{
+        async session({ session,token }) {
+            if(token){
+                session.user._id=token._id;
+                session.user.isVerified=token.isVerified;
+                session.user.isAcceptingMessage=token.isAcceptingMessage;
+                session.user.username=token.username;
+            }
+            return session
+          },
+          async jwt({ token, user}) {
+            if (user) {
+              token._id = user._id?.toString();
+              token.isVerified = user.isVerified;
+              token.isAcceptingMessage = user.isAcceptingMessages;
+              token.username = user.username;
+            }
+            return token
+          }
+    }
+}
---
+++ src/middleware.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+export {default} from 'next-auth/middleware'
+import { getToken } from 'next-auth/jwt'
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+    const token = await getToken({req:request})
+    const url = request.nextUrl;
+
+    // If the user is authenticated
+    if(token) {
+        // and tries to access auth pages or the root, redirect to dashboard
+        if (
+            url.pathname.startsWith('/sign-in') ||
+            url.pathname.startsWith('/signup') ||
+            url.pathname.startsWith('/verify') ||
+            url.pathname === '/'
+        ) {
+            return NextResponse.redirect(new URL('/dashboard', request.url))
+        }
+    } 
+    // If the user is not authenticated
+    else {
+        // and tries to access protected pages, redirect to sign-in
+        if (url.pathname.startsWith('/dashboard')) {
+            return NextResponse.redirect(new URL('/sign-in', request.url));
+        }
+    }
+
+    // Allow the request to continue
+    return NextResponse.next();
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/sign-in',
+    '/signup',
+    '/',
+    '/dashboard/:path*',
+    '/verify/:path*'
+  ]
+}
